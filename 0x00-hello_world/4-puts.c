2a09413bacf17077ad9b4df2ac3d65700203b849
root <root@Hp.localdomain>
2023-03-17 00:33:28 +0100

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
+ 
 /**
-   * main - Entry point
-    *
-     *Description: display a string using puts function
-      *
-       * Return: Always 0 (success)
-        */
+   *main - Entry point
+    *Return: Always 0 (Success)
+     */
+ 
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-
