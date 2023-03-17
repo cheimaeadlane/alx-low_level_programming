6b98449b8eea74c162204ca5fcde9044f5cd87d3
root <root@Hp.localdomain>
2023-03-17 23:49:26 +0100

    positive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Description: print value of n satus; zero, positive or negative
+
  *
  * Return: Always 0 (Success)
  */
