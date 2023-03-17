7a4d641ef0599ecf7e3035ff1bf8e5a3cc9bb69f
root <root@Hp.localdomain>
2023-03-17 23:53:57 +0100

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Description: print value of n satus; zero, positive or negative
-
  *
  * Return: Always 0 (Success)
  */
  int main(void)
-{
+ {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -23,4 +21,4 @@
 	else
 		printf("%i is negative\n", n);
 	return (0);
-}
+ } 
