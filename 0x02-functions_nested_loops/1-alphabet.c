91577452d1eb405ff30344df7545662d755cafbf
root <root@Hp.localdomain>
2023-03-22 21:45:25 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,18 +12,4 @@ void print_alphabet(void)
 	for (ch = 'a'; ch <= 'z'; ++ch)
 		_putchar(ch);
 	_putchar('\n');
-}#include"main.h"
-
-/**
- * print_alphabet - utilizes on the _putchar function to print
- *                 the alphabet a - z
- */
-
-void print_alphabet(void)
-{
-	int ch;
-
-	for (ch = 'a'; ch <= 'z'; ++ch)
-		_putchar(ch);
-	_putchar('\n');
 }
